47ef5c0f83965df0f7ebf66e4658af40d80336f4
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-05-31 10:15:05 +0530

    added morgan dev tool

--- server/app.js
+++ server/app.js
@@ -2,11 +2,16 @@ import  express  from "express";
 import userIndex from "./Routes/userRoute.js"
 import adminIndex from "./Routes/adminRoute.js"
 import dbconnect from "./config/dbconfig.js";
+import morgan from "morgan";
 const app=express()
+dbconnect()
 
+
+
+
+app.use(morgan("dev"))
 app.use('/',userIndex)
 app.use('/admin',adminIndex)
-dbconnect()
 app.listen(4000,()=>{
     console.log("server running");
 })
