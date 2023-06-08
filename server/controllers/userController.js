42e940b91d8c1190d7248179093c7aa01ac39679
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-06-08 19:37:35 +0530

    created user model

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 import asyncHandler from "express-async-handler";
 import jwt from "jsonwebtoken";
 import { bcryptPassword } from "../bcrypt/bcrypt.js";
+import User from "../model/userModel.js";
 
 export const checkUserLoggedIn = asyncHandler((req, res) => {
   const token = req.cookies.token;
@@ -8,10 +9,18 @@ export const checkUserLoggedIn = asyncHandler((req, res) => {
   if (!token) return res.json({ loggedin: false, message: "no token" });
 });
 
-export const userRegister=asyncHandler((req,res)=>{
+export const userRegister=asyncHandler(async(req,res)=>{
 
     const { name, email, password, mobile } = req.body;
-   
-const newPassword= bcryptPassword(password)
-console.log(newPassword,name,email);
+   const user=await User.findOne(email)
+   if(user){
+    return res.json({ error: true, message: "User Already Exist" })
+   }else{
+
+       const newPassword= bcryptPassword(password)
+       const newUser=User.create({name, email, newPassword, mobile})
+       console.log(newUser);
+   }
+
+
 })
