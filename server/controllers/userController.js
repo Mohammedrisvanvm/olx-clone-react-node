e34143f26bcfc0872ef49c0155d17d1261cbbcbb
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-06-08 18:17:29 +0530

    bcrypt separate file created

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,8 @@
 import asyncHandler from "express-async-handler";
-import jwt from 'jsonwebtoken'
-export const checkUserLoggedIn=asyncHandler((req,res)=>{
-    const token=req.cookies.token
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+export const checkUserLoggedIn = asyncHandler((req, res) => {
+  const token = req.cookies.token;
 
-    if(!token) res.json({loggedin:false,message: "no token"})
-})
-
+  if (!token) res.json({ loggedin: false, message: "no token" });
+});
